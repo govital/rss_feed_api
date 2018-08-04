c4c0b12e44e19d29f1aa174c80126a8c4478ed09
govital3 <govital3@gmail.com>
2018-08-04 15:29:52 +0300

    first push

---
+++ src/main/java/com/example/realcomerce/RealcomerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.realcomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealcomerceApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealcomerceApplication.class, args);
+	}
+}
